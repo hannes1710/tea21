09a980ab73acf3e6461d8b0182331b773341596d
hannes1710 <149794537+hannes1710@users.noreply.github.com>
2023-11-24 15:10:32 +0100

    .

--- exercise-004/main.cpp
+++ exercise-004/main.cpp
@@ -3,9 +3,14 @@
 
 #include "CLI/CLI.hpp"
 #include "config.h"
+#include <iostream>
+
+#include "BMP.h"
 
 auto main(int argc, char **argv) -> int
 {
+    std::string filename(DEFAULT_BMP_FILE);
+
     /**
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
@@ -14,6 +19,7 @@ auto main(int argc, char **argv) -> int
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
+        app.add_option("-f,--file", filename, fmt::format("Create a vector with the given size default: {}", filename));
         app.parse(argc, argv);
     }
     catch (const CLI::ParseError &e)
@@ -27,8 +33,32 @@ auto main(int argc, char **argv) -> int
      * More info at https://fmt.dev/latest/api.html
      */
     fmt::print("Hello, {}!\n", app.get_name());
+    fmt::print("The filename to use {}\n", filename);
 
-    /* INSERT YOUR CODE HERE */
+    BMP bmp;
+    bmp.read(filename);
+
+
+    const std::string ascii = " .:-=+*#%@";
+
+
+    for (int i = 0; i < bmp.infoHeader.imageHeight; i++){
+        for (int j = 0; j < bmp.infoHeader.imageWidth, j++){
+
+            int index = static_cast<int>((bmp.imageData[i*j]/255))*(ascii.size()-1);
+            fmt::print("{}",ascii[index]);
+
+        }
+        fmt::print("\n");
+    }
+    
+
+
+    for (int i = 0; i < bmp.fileHeader.fileSize; ++i) {
+        //pixel* currentPixel = reinterpret_cast<pixel*>(&bmp.imageData[i * sizeof(pixel)]);
+        //std::cout << "Pixel " << i << ": R=" << (int)currentPixel->red << ", G=" << (int)currentPixel->green << ", B=" << (int)currentPixel->blue << std::endl;
+        fmt::println("datenpunkt {}", bmp.imageData[i]);
+    }
 
     return 0; /* exit gracefully*/
 }
